622e2ed91966f7019c2cab306325b71ba02a1914
reg-era <ilyass.med.fkh@gmail.com>
2025-02-01 03:24:28 +0100

    get project xp

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # graph
+
+## Type of quering 
+```
+    Simple Query (Basic retrieval)
+    Query with Variables (Dynamic input)
+    Query by Condition (Filtering)
+    Query with Pagination (Limit and offset or cursor-based pagination)
+    Query with Sorting (Order results by fields)
+    Query with Nested Relationships (Similar to SQL joins)
+    Query with Aliases (Renaming results to avoid conflicts)
+    Query with Fragments (Reusable pieces of queries)
+```
--- assets/js/lib/interfaceQL.js
+++ assets/js/lib/interfaceQL.js
@@ -1,29 +1,13 @@
-import { fetchData } from "./expo.js"
-
-const getUserXP = async () => {
-    const query = `{
-        user {
-            firstName
-            lastName
-            id
-            auditRatio
+const getProjectXP = `
+{
+    transaction(where: {type: {_eq: "xp"}, object: {type: {_eq: "project"}}}) {
+        amount
+        createdAt
+        object {
+            name
         }
-        transaction{
-            amount
-        }
-    }`
-
-    const data = await fetchData(query)
-    const res = data.data["transaction"]
-    console.log(res[0].amount);
-    let acc = 0
-    for (let i=0 ;i<res.length;i++){
-        acc+=res[i].amount
     }
-    console.log(acc);
-    
+}`
 
 
-}
-
-export { getUserXP }
+export { getProjectXP }
